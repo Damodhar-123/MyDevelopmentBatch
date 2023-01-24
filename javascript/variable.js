2c6e53e0a00a662043000aa7f9e230430b027102
Damodhar-123 <maskedamodhar@gmail.com>
2023-01-24 15:13:34 +0530

    intro of var let and const

---
+++ javascript/variable.js
@@ -0,0 +1,18 @@
+let a;
+a = 20;
+let b = 30;
+console.log(b);
+let favFruit = 'mango';
+console.log(favFruit);
+console.log(a);
+
+var favCar = 'nexon';
+console.log(favCar);
+var favCar = 'taigun';
+console.log(favCar);
+
+const myName = 'Damodhar';
+console.log(myName);
+console.log(myName);
+const moNumber = '9763330729';
+console.log(moNumber);
