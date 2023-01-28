2ff266f8687e6020f7f3c7a80e1565931e8d4000
Damodhar-123 <maskedamodhar@gmail.com>
2023-01-28 19:52:52 +0530

    test case of console

---
+++ javascript/consoletest.js
@@ -0,0 +1,18 @@
+let myFirstName = "Damodhar";
+let mySurname = "Maske";
+console.log(myFirstName);
+console.log(mySurname , myFirstName);
+let num1;
+let num2;
+let num3;
+num1 = 30;
+num2 = 20;
+num3 = 60;
+console.log(num1 + num2);
+console.log(num1 + num2 + num3 );
+let num4 = 40;
+let num5 = 100;
+console.log(num1 + num2 +num5 + myFirstName + num3 + num4);
+console.log(num1 + num2 + " " + mySurname + ' ' + num5);
+console.log(myFirstName , mySurname , num1 , num4 ,num5);
+console.log(num1 + num3 + num5 , mySurname , myFirstName , num4 , num2);
