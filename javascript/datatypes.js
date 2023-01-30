beb64f0773104ce3699a2b431bc8e1feb1138a3c
Damodhar-123 <maskedamodhar@gmail.com>
2023-01-30 21:31:36 +0530

    arrays and objects

---
+++ javascript/datatypes.js
@@ -0,0 +1,45 @@
+// let myName = 'Damodhar';
+
+let myName = [ 'Damodhar' , 'Maske', 5120 , 'raj' , 1122 , 'sd']
+console.log(myName.length);
+console.log(myName[0]);
+console.log(myName[5]);
+
+let studentList = {
+    Name: 'Rahul',
+    Class: 12,
+    Division: 'A',
+    PassStatus: 'Pass'
+}
+console.log(studentList);
+console.log(studentList.Name);
+
+let employees = [
+    {
+        Name: 'Sandip' ,
+        Age: 28,
+        Dep: 'QA',
+        Sex: 'Male' 
+    },
+    {
+        Name: 'Vijay' ,
+        Age: 25,
+        Dep: 'QA' ,
+        Sex: 'Male'  
+    },
+    {
+        Name: 'Vidhya' ,
+        Age: 29,
+        Dep: 'Hr' ,
+        Sex: 'Female' 
+    },
+    {
+        Name: 'Samarth',
+        Age: 31,
+        Dep: 'PD' ,
+        Sex: 'Male' 
+    }
+]
+console.log(employees[0].Name);
+console.log(employees[2].Name);
+console.log(employees.length);
