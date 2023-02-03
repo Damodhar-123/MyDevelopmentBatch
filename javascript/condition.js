7c949d09a123af55659e9d62836d00d44d02ca27
Damodhar-123 <maskedamodhar@gmail.com>
2023-02-03 21:58:59 +0530

    learn if and else if conditions

---
+++ javascript/condition.js
@@ -0,0 +1,21 @@
+
+
+var num = 60;
+if (num > 80 ) {
+    console.log('num is greater');
+}
+else {
+    console.log('num is smaller');
+}
+if (num == 50) {
+    console.log(num);
+}
+else if (num == 30) {
+    console.log(num);
+}
+else if (num == 60) {
+    console.log(num);
+}
+else {
+    console.log(num);
+}
