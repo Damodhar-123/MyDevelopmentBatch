81ba4ebcd7dd5f823f8006051f8dfce1b0417a20
Damodhar-123 <maskedamodhar@gmail.com>
2023-02-05 12:41:25 +0530

    add switch statement

--- javascript/condition.js
+++ javascript/condition.js
@@ -1,4 +1,5 @@
 
+// if statement:
 
 var num = 60;
 if (num > 80 ) {
@@ -17,5 +18,46 @@ else if (num == 60) {
     console.log(num);
 }
 else {
-    console.log(num);
+    console.log( 'else block--->',num);
 }
+
+// switch statement :
+
+switch (num) {
+    case 40 :
+        var res = 50+60;
+        console.log(res);
+        break;
+    case 55:
+        console.log(res);
+        break;
+    case 60 :
+        var res = 50+40;
+        console.log(res);
+        break;      
+
+    default:
+        console.log('default');
+        break;
+}
+
+
+//switch-statement:
+// switch (number) {
+//     case  40 :
+//         let res = 50+ 60;
+//         console.log(res); 
+//         break;
+//     case  60:
+//         console.log(number); 
+//         break;
+//     case 50:
+//         console.log(number); 
+//     case 80:
+//         console.log(number); 
+    
+
+//     default:
+//         console.log('default');
+//      break;
+// }
