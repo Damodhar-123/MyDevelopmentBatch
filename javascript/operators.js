4a0110b9870a511a9378d07fa5ef0ece7293574e
Damodhar-123 <maskedamodhar@gmail.com>
2023-02-05 14:01:03 +0530

    arthematic logical and incremental and decremental

---
+++ javascript/operators.js
@@ -0,0 +1,42 @@
+// arithematic operators:
+var x = 50;
+var y = 60;
+x = x + y;
+console.log(x);
+
+// postfix operator and prefix operators:
+// incremental operator:
+let value1 = 20;
+console.log(value1);
+console.log(value1++);
+console.log(value1);
+console.log(value1++);
+console.log(value1);
+
+let value2 = 30;
+console.log(value2);
+console.log(++value2);
+console.log(value2);
+console.log(++value2);
+console.log(value2);
+
+// decremental operator:
+
+let value3 = 40;
+console.log(value3);
+console.log(value3--);
+console.log(value3);
+
+let value4= 50;
+console.log(value4);
+console.log(--value4);
+console.log(--value4);
+
+// logical operators:
+
+var x= 40;
+var y= 30;
+
+if (x > 35 && y < 35) {
+    console.log("okokokokok");
+}
