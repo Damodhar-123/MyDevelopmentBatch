f851c6f9f8f42f6b61a4674773b17fd640725255
Damodhar-123 <maskedamodhar@gmail.com>
2023-02-18 12:14:17 +0530

    add number methods

--- javascript/numberMethod.js
+++ javascript/numberMethod.js
@@ -1,3 +1,17 @@
-var data =  150 + 100;
+var data = 150 + 100;
 let convData = data.toString();
 console.log(convData);
+
+let x = 1.56565656;
+console.log(x.toFixed(2));    // this tofixed method used fixed value after any index of after decimal point
+
+let a = 2.56565658755;
+let y = a.valueOf()         // this method use to assign value one variable to another variable
+console.log(y);
+
+let num = '12345';
+let num1 = parseInt(num);   // this method use to change string value to number
+console.log(num1);
+
+let xyz = 11.1888881212165423454; console.log(xyz.toPrecision(5));
+   // this method use to fixed decimal value at some index from starting point
